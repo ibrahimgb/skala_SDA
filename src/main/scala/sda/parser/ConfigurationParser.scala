8b56792bf767108412e5455b866f89684927b368
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-05-31 19:04:07 +0200

    read using file url

--- src/main/scala/sda/parser/ConfigurationParser.scala
+++ src/main/scala/sda/parser/ConfigurationParser.scala
@@ -1,5 +1,6 @@
 package sda.parser
-
+import scala.io.Source
+import upickle.default._
 import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods
 import sda.reader._
@@ -9,14 +10,17 @@ object ConfigurationParser {
   // Implicit formats for JSON serialization/deserialization
   implicit val formats = DefaultFormats
 
-  def getCsvReaderConfigurationFromJson(jsonString: String): CsvReader = {
+  def getCsvReaderConfigurationFromJson(jsonUrl: String): CsvReader = {
+    val jsonString = FileReaderUsingIOSource.getContent(jsonUrl)
     JsonMethods.parse(jsonString).extract[CsvReader]
   }
-
   /* Complétez cette fonction. Elle prend un String en argument et renvoie un objet JsonReader.
      Elle doit être codée de la même manière que la fonction getCsvReaderConfigurationFromJson // Done
   */
-  def getJsonReaderConfigurationFromJson(jsonString: String): JsonReader = {
-    JsonMethods.parse(jsonString).extract[JsonReader]
+
+  def getJsonReaderConfigurationFromJson(jsonUrl: String) = {
+     val jsonString = FileReaderUsingIOSource.getContent(jsonUrl)
+    println(jsonString)
+     JsonMethods.parse(jsonString).extract[JsonReader]
   }
 }
